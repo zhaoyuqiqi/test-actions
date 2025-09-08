d90bbe51e56e76d77385fce0bc953daddf7d6238
赵一一 <1940988349@qq.com>
2025-09-08 23:28:08 +0800

    feat: add log

--- log.ts
+++ log.ts
@@ -1,2 +1,10 @@
-const { logid, aaa } = process.env
-console.log(logid, aaa);
+import { sleep } from 'bun';
+
+const { logid, aaa } = process.env;
+async function main() {
+  await sleep(10000);
+  console.log(logid, aaa);
+  await sleep(2000);
+  console.log('通知服务端，任务执行完毕');
+}
+main();
